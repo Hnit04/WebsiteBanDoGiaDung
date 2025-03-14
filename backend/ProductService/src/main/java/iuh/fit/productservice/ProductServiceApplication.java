0a08d34818b8ed602b5dbfee30261bcde87f0c3c
Trần Công Tính <165116050+0326829327@users.noreply.github.com>
2025-03-14 23:59:27 +0700

    comit

--- backend/ProductService/src/main/java/iuh/fit/productservice/ProductServiceApplication.java
+++ backend/ProductService/src/main/java/iuh/fit/productservice/ProductServiceApplication.java
@@ -2,12 +2,12 @@ package iuh.fit.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "iuh.fit.productservice.repository") // Đảm bảo dòng này có
 public class ProductServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
-
 }
